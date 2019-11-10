d8711d5a2b39203c9998e930df5f91b9756cfe25
yel1ow <lv.reg.all@gmail.com>
2019-11-10 18:38:40 +0300

    BCV-2 Added CSV parser

---
+++ src/main/java/dsx/bcv/services/CSVParserService.java
@@ -0,0 +1,86 @@
+package dsx.bcv.services;
+
+import dsx.bcv.data.models.Deal;
+import dsx.bcv.data.models.Transaction;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVParserService {
+
+    public List<Deal> parseDeals(Reader in) throws IOException {
+        return parseDeals(in, ',');
+    }
+
+    public List<Deal> parseDeals(Reader in, char separator) throws IOException {
+
+        List<Deal> deals = new ArrayList<>();
+
+        Iterable<CSVRecord> records = CSVFormat.newFormat(separator).parse(in);
+        for (CSVRecord record : records) {
+            deals.add(getDealFromCSVRecord(record));
+        }
+
+        return deals;
+    }
+
+    private Deal getDealFromCSVRecord(CSVRecord record) {
+
+        final LocalDateTime dateTime = getDateTimeFromString(record.get(0));
+        final String instrument = record.get(1);
+        final String dealType = record.get(2);
+        final BigDecimal tradedQuantity = new BigDecimal(record.get(3));
+        final String tradedQuantityCurrency = record.get(4);
+        final BigDecimal tradedPrice = new BigDecimal(record.get(5));
+        final String tradedPriceCurrency = record.get(6);
+        final BigDecimal commission = new BigDecimal(record.get(7));
+        final String commissionCurrency = record.get(8);
+        final BigInteger tradeValueId = new BigInteger(record.get(9));
+
+        return new Deal(dateTime, instrument, dealType, tradedQuantity, tradedQuantityCurrency,
+                tradedPrice, tradedPriceCurrency, commission, commissionCurrency, tradeValueId);
+    }
+
+    public List<Transaction> parseTransactions(Reader in) throws IOException {
+        return parseTransactions(in, ',');
+    }
+
+    public List<Transaction> parseTransactions(Reader in, char separator) throws IOException {
+
+        List<Transaction> transactions = new ArrayList<>();
+
+        Iterable<CSVRecord> records = CSVFormat.newFormat(separator).parse(in);
+        for (CSVRecord record : records) {
+            transactions.add(getTransactionFromCSVRecord(record));
+        }
+
+        return transactions;
+    }
+
+    private Transaction getTransactionFromCSVRecord(CSVRecord record) {
+
+        final LocalDateTime dateTime = getDateTimeFromString(record.get(0));
+        final String transactionType = record.get(1);
+        final String currency = record.get(2);
+        final BigDecimal amount = new BigDecimal(record.get(3));
+        final BigDecimal commission = new BigDecimal(record.get(4));
+        final String transactionStatus = record.get(5);
+        final BigInteger transactionValueId = new BigInteger(record.get(6));
+
+        return new Transaction(dateTime, transactionType, currency, amount, commission,
+                transactionStatus, transactionValueId);
+    }
+
+    private LocalDateTime getDateTimeFromString(String string) {
+
+        final String localDateTimeFormat = string.replace(' ', 'T');
+        return LocalDateTime.parse(localDateTimeFormat);
+    }
+}
