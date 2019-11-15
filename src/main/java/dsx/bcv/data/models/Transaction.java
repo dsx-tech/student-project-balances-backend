663af13dc5418348743db147763597652cb82a99
yel1ow <lv.reg.all@gmail.com>
2019-11-15 23:41:23 +0300

    BCV-2 refactor Trade, Transaction to @Lombok.Data classes

--- src/main/java/dsx/bcv/data/models/Transaction.java
+++ src/main/java/dsx/bcv/data/models/Transaction.java
@@ -1,75 +1,30 @@
 package dsx.bcv.data.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
+@Data
+@AllArgsConstructor
+@Builder
 public class Transaction {
 
-    public Transaction(LocalDateTime dateTime, String transactionType,
-                       String currency, BigDecimal amount, BigDecimal commission,
-                       String transactionStatus, long transactionValueId) {
-            this.dateTime = dateTime;
-            this.transactionType = transactionType;
-            this.currency = currency;
-            this.amount = amount;
-            this.commission = commission;
-            this.transactionStatus = transactionStatus;
-            this.transactionValueId = transactionValueId;
-        }
+    @NonNull
+    private LocalDateTime dateTime;
+    @NonNull
+    private String transactionType;
+    @NonNull
+    private String currency;
+    @NonNull
+    private BigDecimal amount;
+    @NonNull
+    private BigDecimal commission;
+    @NonNull
+    private String transactionStatus;
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public String getTransactionType() {
-        return transactionType;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public BigDecimal getCommission() {
-        return commission;
-    }
-
-    public String getTransactionStatus() {
-        return transactionStatus;
-    }
-
-    public long getTransactionValueId() {
-        return transactionValueId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Transaction that = (Transaction) o;
-        return transactionValueId == that.transactionValueId &&
-                dateTime.equals(that.dateTime) &&
-                transactionType.equals(that.transactionType) &&
-                currency.equals(that.currency) &&
-                amount.equals(that.amount) &&
-                commission.equals(that.commission) &&
-                transactionStatus.equals(that.transactionStatus);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(dateTime, transactionType, currency, amount, commission, transactionStatus, transactionValueId);
-    }
-
-    private final LocalDateTime dateTime;
-    private final String transactionType;
-    private final String currency;
-    private final BigDecimal amount;
-    private final BigDecimal commission;
-    private final String transactionStatus;
-    private final long transactionValueId;
+    private long transactionValueId;
 }
