42ac8b8eb100036adfea642581892ee88e82a2da
Ilya Reshetnikov <37152998+Rimalon@users.noreply.github.com>
2019-11-24 16:29:25 +0300

    BCV-3 Added overloaded methods

--- marketdata-provider/src/main/java/dsx/bcv/services/QuoteProvider.java
+++ marketdata-provider/src/main/java/dsx/bcv/services/QuoteProvider.java
@@ -1,5 +1,6 @@
 package dsx.bcv.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.InjectableValues;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,33 +11,39 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class QuoteProvider {
     private final URLFetcher fetcher;
-    private final List<Tiker> actualData;
+    private volatile List<Tiker> actualData;
     private final ObjectMapper mapper;
+    private final Formatter formatter;
 
-    public QuoteProvider() throws IOException {
+    QuoteProvider() throws IOException {
         this.mapper = new ObjectMapper();
         this.fetcher = new URLFetcher();
+        this.formatter = new Formatter();
         this.actualData = receiveActualData();
     }
 
-    public List<Tiker> getActualData(){
-        synchronized(actualData){
-            return actualData;
-        }
+    public List<Tiker> getActualData() {
+        return actualData;
+    }
+
+    private List<Tiker> receiveActualData(List<String> idsList) throws IOException {
+        String response = fetcher.getDsxTikers(idsList);
+        return parseActualData(response);
     }
 
     private List<Tiker> receiveActualData() throws IOException {
         String response = fetcher.getDsxTikers();
+        return parseActualData(response);
+    }
+
+    private List<Tiker> parseActualData(String response) throws IOException {
         JsonNode rootNode = mapper.readValue(response, JsonNode.class);
         List<Tiker> result = new ArrayList<>();
         var iterator = rootNode.fieldNames();
@@ -47,45 +54,77 @@ public class QuoteProvider {
         return result;
     }
 
+    public void startReceivingActualData(List<String> idsList) {
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleAtFixedRate(() -> {
+            try {
+                this.actualData = receiveActualData(idsList);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }, 0, 60, TimeUnit.SECONDS);
+    }
+
     public void startReceivingActualData() {
         ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
         service.scheduleAtFixedRate(() -> {
-            synchronized(actualData){
-                try {
-                    actualData.clear();
-                    actualData.addAll(receiveActualData());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            try {
+                this.actualData = receiveActualData();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }, 0, 60, TimeUnit.SECONDS);
     }
 
     private List<Bar> getHistoricalData(int amount) throws IOException {
         String response = fetcher.getDsxLastBars(amount);
+        return parseHistoricalData(response);
+    }
+
+    private List<Bar> getHistoricalData(String period, int amount) throws IOException {
+        String response = fetcher.getDsxLastBars(period, amount);
+        return parseHistoricalData(response);
+    }
+
+    private List<Bar> getHistoricalData(List<String> idsList, String period, int amount) throws IOException {
+        String response = fetcher.getDsxLastBars(idsList, period, amount);
+        return parseHistoricalData(response);
+    }
+
+    private List<Bar> parseHistoricalData(String response) throws IOException {
         JsonNode rootNode = mapper.readValue(response, JsonNode.class);
-        ArrayList<Bar> results =  new ArrayList<Bar>();
+        ArrayList<Bar> result =  new ArrayList<Bar>();
         var iterator = rootNode.fieldNames();
         while (iterator.hasNext()){
             String id = iterator.next();
             InjectableValues inject = new InjectableValues.Std().addValue(String.class, id);
-            results.addAll(Arrays.asList(mapper.reader(inject).forType(Bar[].class).readValue(rootNode.get(id))));
+            result.addAll(Arrays.asList(mapper.reader(inject).forType(Bar[].class).readValue(rootNode.get(id))));
         }
-        results.sort(Comparator.comparingLong(bar -> ((Bar)bar).getTimestamp()).thenComparing(bar -> ((Bar)bar).getId()));
-        return results;
+        result.sort(Comparator.comparingLong(bar -> ((Bar)bar).getTimestamp()).thenComparing(bar -> ((Bar)bar).getId()));
+        return result;
     }
 
-    public void printHistoricalData(int amount, String outputPath) throws IOException {
-        printHistoricalData(amount, outputPath, ',');
-    }
-
-    private void printHistoricalData(int amount, String outputPath, char separator) throws IOException {
+    void printHistoricalData(int amount, String outputPath) throws IOException {
         List<Bar> bars = getHistoricalData(amount);
+        printBarsToFile(bars,outputPath);
+    }
+
+    void printHistoricalData(String period, int amount, String outputPath) throws IOException {
+        List<Bar> bars = getHistoricalData(period, amount);
+        printBarsToFile(bars,outputPath);
+    }
+
+    void printHistoricalData(List<String> idsList,String period, int amount, String outputPath) throws IOException {
+        List<Bar> bars = getHistoricalData(idsList, period, amount);
+        printBarsToFile(bars,outputPath);
+    }
+
+    private void printBarsToFile(List<Bar> bars, String outputPath) throws IOException {
         FileOutputStream out = new FileOutputStream(outputPath);
         OutputStreamWriter writer = new OutputStreamWriter(out);
-        CSVPrinter printer = new CSVPrinter(writer, CSVFormat.newFormat(separator));
+        CSVPrinter printer = new CSVPrinter(writer, CSVFormat.newFormat(','));
         for (var bar : bars) {
-                printer.printRecord( bar.toCsvRecord());
+            printer.printRecord( bar.toCsvRecord(this.formatter));
 
         }
         printer.close();
